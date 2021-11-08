09ea00bd7a661a8a9cb2e786a429f3ac59d0f2b8
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-08 11:45:46 +0100

    integrate the home component in the routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
