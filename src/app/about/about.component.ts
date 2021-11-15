8a47e22e6ec506669dc70da8b84c675758f0f678
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-15 12:46:26 +0100

    databinging examples

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public user1 = {
+    name: 'John Doe',
+    age: 32,
+    group: "DSI33",
+    email: 'john.doe.gmail.com',
+    phone: '+33 6 12 34 56 78',
+    activeStyle: {
+      color: 'red',
+      fontWeight: 'bold'
+    }
+  };
+  public user2 = {
+    name: 'Jane Doe',
+    age: 29,
+    group: "DSI32",
+    email: 'jane.doe.gmail.com',
+    phone: '+33 6 43 34 78 90',
+    activeStyle : {
+      color: 'blue',
+      fontStyle: 'italic'
+    }
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public switchStyle() {
+    const color = this.user1.activeStyle.color;
+    this.user1.activeStyle.color = this.user2.activeStyle.color;
+    this.user2.activeStyle.color = color;
+    alert('Style changed');
   }
 
 }
