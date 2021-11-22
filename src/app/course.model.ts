7f359314c55a2fd78b1793338915a5133d85b313
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-22 12:17:25 +0100

    create course interface

---
+++ src/app/course.model.ts
@@ -0,0 +1,12 @@
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  last_update: string;
+  instructor: string;
+  language: string;
+  price: number;
+  goals: string[];
+  requirements: string[];
+}
