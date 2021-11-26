28738572ad93e81901cb35304a8ffeecf1d9c77b
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-26 20:01:00 +0100

    update image link

--- src/app/courses-list.ts
+++ src/app/courses-list.ts
@@ -6,7 +6,7 @@ export const courses: Course[] = [
     description:
       'This course introduces students to the basic concepts of computer science. It covers the basic programming concepts, data structures, and algorithms, and introduces students to the software development process. Students will learn to use a programming language, and to write algorithms in a programming language. This course is a prerequisite for the CS program.',
     image:
-      'https://scontent.ftun17-1.fna.fbcdn.net/v/t1.18169-9/26167375_1635106093214957_5060003631117229952_n.png?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=tX0GHOqZ-g4AX-FEBuo&_nc_ht=scontent.ftun17-1.fna&oh=d84bfc3a25c7acdf262a36e1a01ed523&oe=619D30D7',
+      'https://scontent.ftun16-1.fna.fbcdn.net/v/t1.18169-9/26167375_1635106093214957_5060003631117229952_n.png?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=ED2j9blggBMAX-7gLra&_nc_ht=scontent.ftun16-1.fna&oh=45a4f5642ba5fdc7aa81001e55aa173f&oe=61C4BDD7',
     last_update: '10/2020',
     instructor: 'Samuel Spencer',
     language: 'English',
