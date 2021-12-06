56e32b6398d624ba04b279d44540de29a174c90d
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-06 11:38:33 +0100

    get cart content when checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from "src/app/services/cart.service";
 
+import { courses } from "src/app/courses-list";
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -14,6 +16,11 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.cartContent = this.cartService.cartContent;
+    for (let index = 0; index < this.cartContent.length; index++) {
+      const course = courses.filter(course => course.id == this.cartContent[index].id)[0];
+      this.cartContent[index].title = course.title;
+      this.cartContent[index].price = course.price;
+    }
   }
 
 }
