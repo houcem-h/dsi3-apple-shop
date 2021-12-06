47d6d8e52c59319059c3b13bb8eb6a1e1ef022a6
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-06 13:29:15 +0100

    pass data from child to parent component

--- src/app/courses/course-item/course-item.component.ts
+++ src/app/courses/course-item/course-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-course-item',
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CourseItemComponent implements OnInit {
 
   @Input() courseItem: any;
+  @Output() addToCart = new EventEmitter();
 
   constructor() { }
 
