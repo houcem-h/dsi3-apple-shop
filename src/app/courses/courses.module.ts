579c4f43c346e828ae40725b022c10e7c56aaf79
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-06 13:07:26 +0100

    use course item component to display courses list

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses.component';
+import { CourseItemComponent } from './course-item/course-item.component';
 
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseItemComponent
   ],
   imports: [
     CommonModule,
