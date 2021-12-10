b2c2bfd7e99e0099dfda6b22376f67ec7bf2200d
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-10 10:02:58 +0100

    ng g c auth/login -m auth-routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [{ path: '', component: AuthComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    LoginComponent
+  ]
 })
 export class AuthRoutingModule { }
