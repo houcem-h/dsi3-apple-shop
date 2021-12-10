5e1153b796002214dd5a969f9192de9dc8268862
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-10 10:57:57 +0100

    signup method

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,40 @@
 import { Injectable } from '@angular/core';
 
-import { HttpHeaders } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpErrorResponse  } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { User } from 'src/app/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  API_URL: string = 'http://localhost:4000';
+  API_URL: string = 'http://localhost:3000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  signup(user: User): Observable<any> {
+
+    return this.httpClient.post(`${this.API_URL}/auth/signup`, user).pipe(
+        catchError(this.handleError)
+    )
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      msg = error.error.message;
+    } else {
+      // server-side error
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(msg);
+  }
 }
