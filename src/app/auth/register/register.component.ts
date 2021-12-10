ae09be305ffeb13a98473b931eb35364978d57e5
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-10 19:36:59 +0100

    signup form and validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -14,14 +14,30 @@ export class RegisterComponent implements OnInit {
     public formBuilder: FormBuilder,
   ) {
     this.signupForm= this.formBuilder.group({
-      name: [''],
-      email: [''],
-      password: [''],
-      confirm_password: ['']
-    })
+      name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.minLength(6), Validators.required]],
+      confirm_password: ['', [Validators.minLength(6), Validators.required]],
+    }, {validator: this.passwordMatchValidator('password', 'confirm_password')}
+    )
    }
 
   ngOnInit(): void {
+    console.log(this.signupForm);
+  }
+
+  signupUser() {
+    console.log(this.signupForm);
+  }
+
+  private passwordMatchValidator(password: string, confirm_password: string) {
+    return (group: FormGroup) => {
+      const passwordInput = group.controls[password];
+      const confirmPasswordInput = group.controls[confirm_password];
+      if (passwordInput.value !== confirmPasswordInput.value) {
+        return confirmPasswordInput.setErrors({ NoPassswordMatch: true });
+      }
+    }
   }
 
 }
