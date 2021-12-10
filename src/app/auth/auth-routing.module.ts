c36f52594a6200f3b4f5242ce74948cdb5aa75b6
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-10 20:10:55 +0100

    display error messages

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
+import { CommonModule } from "@angular/common";
 
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
@@ -16,7 +17,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   exports: [RouterModule],
   declarations: [
