77e500f08bf7c3a002498fd5d5aff9bbd8114350
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-12 09:19:56 +0100

    login method in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 import { HttpHeaders, HttpClient, HttpErrorResponse  } from '@angular/common/http';
+import { Router } from "@angular/router";
 
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+
+import { LocalStorageService } from "src/app/services/local-storage.service";
 
 import { User } from 'src/app/user.model';
 
@@ -14,15 +17,38 @@ export class AuthService {
 
   API_URL: string = 'http://localhost:3000/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
+  currentUser = {};
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    public router: Router,
+    private localStorageService: LocalStorageService
   ) { }
 
   signup(user: User): Observable<any> {
 
     return this.httpClient.post(`${this.API_URL}/auth/signup`, user).pipe(
         catchError(this.handleError)
+    )
+  }
+
+  login(user: User) {
+    return this.httpClient.post<any>(`${this.API_URL}/auth/login`, user)
+      .subscribe((res: any) => {
+        this.localStorageService.set('access_token', res.token);
+        this.getUserProfile(res.userId).subscribe((res) => {
+          this.currentUser = res;
+          this.router.navigate(['users/profile/' + res.msg._id]);
+        })
+      })
+  }
+
+  getUserProfile(id: string): Observable<any> {
+    return this.httpClient.get(`${this.API_URL}/auth/profile/${id}`, { headers: this.headers }).pipe(
+      map((res: Object) => {
+        return res || {}
+      }),
+      catchError(this.handleError)
     )
   }
 
