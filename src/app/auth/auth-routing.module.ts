2333bf596dfc8c472267c1053ed04cc7828616c1
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-19 13:58:37 +0100

    use is auth guard

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -6,11 +6,12 @@ import { CommonModule } from "@angular/common";
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { IsAuthGuard } from '../is-auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'login', component: LoginComponent, canActivate: [IsAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [IsAuthGuard] },
 ];
 
 @NgModule({
