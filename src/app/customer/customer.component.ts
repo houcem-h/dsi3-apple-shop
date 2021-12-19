6ed94c42dfd3e3284a6bd92a134a6c01e9a6efe9
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-19 11:54:04 +0100

    get customer details

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: 'app-customer',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  public customer: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const customerId = params.get("id") || "";
+      this.authService.getUserProfile(customerId).subscribe(customer => this.customer = customer);
+    });
   }
 
 }
