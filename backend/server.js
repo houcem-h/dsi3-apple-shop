9fc3069518312d8e84801628bb0e40850e401c2a
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-22 10:07:43 +0100

    npm i -g nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello DSI3');
+  res.end('Hello DSI3 to MEAN stack course');
 });
 
 server.listen(3000);
