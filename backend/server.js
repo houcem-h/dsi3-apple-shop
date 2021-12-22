f6da8d156364f1f12642a68ebb3b30172b584ab9
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-22 10:16:16 +0100

    update server to use express's app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-  res.end('Hello DSI3 to MEAN stack course');
-});
+app.set('port', process.env.PORT || 3000);
+
+const server = http.createServer(app);
 
 server.listen(3000);
