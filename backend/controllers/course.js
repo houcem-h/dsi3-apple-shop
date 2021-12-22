b2625aeec22eae0d80d98bcac13fdfb99a9e12a7
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-22 11:34:00 +0100

    use a controller

---
+++ backend/controllers/course.js
@@ -0,0 +1,39 @@
+const Course = require('./../models/Course');
+
+// get all courses
+exports.all = (req, res) => {
+  Course.find()
+    .then(courses => res.status(200).json(courses))
+    .catch(err => res.status(400).json({error: err.message}));
+};
+
+// get a course by id
+exports.get = (req, res, next) => {
+  Course.findOne({ _id: req.params.id })
+      .then(course => res.status(200).json(course))
+      .catch(error => res.status(404).json({ error }));
+  };
+
+  // store a new course
+exports.create = (req, res, next) => {
+  const course = new Course({
+    ...req.body
+  });
+  course.save()
+    .then(() => res.status(201).json({ message: 'Course created  !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// update a course by id
+exports.update = (req, res, next) => {
+  Course.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Course updated !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// delete a course by id
+exports.delete = (req, res, next) => {
+  Course.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Course deleted !'}))
+    .catch(error => res.status(400).json({ error }));
+};
--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,44 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const Course = require('./../models/Course');
+const courseController = require('./../controllers/course')
 
-// get all courses
-router.get('/', (req, res) => {
-  Course.find()
-    .then(courses => res.status(200).json(courses))
-    .catch(err => res.status(400).json({error: err.message}));
-});
-
-// get a course by id
-router.get('/:id', (req, res, next) => {
-  Course.findOne({ _id: req.params.id })
-      .then(course => res.status(200).json(course))
-      .catch(error => res.status(404).json({ error }));
-  });
-
-  // store a new course
-router.post('/', (req, res, next) => {
-  const course = new Course({
-    ...req.body
-  });
-  course.save()
-    .then(() => res.status(201).json({ message: 'Course created  !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// update a course by id
-router.put('/:id', (req, res, next) => {
-  Course.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Course updated !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// delete a course by id
-router.delete('/:id', (req, res, next) => {
-  Course.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Course deleted !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+router.get('/', courseController.all);
+router.get('/:id', courseController.get);
+router.post('/', courseController.create);
+router.put('/:id', courseController.update);
+router.delete('/:id', courseController.delete);
 
 module.exports = router;
