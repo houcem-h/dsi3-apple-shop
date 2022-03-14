366df8744d59612483208c679dd69c5c7f99c8f4
Houcem Hedhly <hedhoucem@gmail.com>
2022-03-14 14:01:45 +0100

    indent code

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,11 @@ mongoose.connect('mongodb://localhost:27017/dsi3-2021',
   app.use(express.json());
 
   app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use('/api/courses', courseRouter);
 app.use('/api/auth', userRouter);
